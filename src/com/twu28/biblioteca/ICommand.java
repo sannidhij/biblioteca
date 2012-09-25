e517632abdbbd747015ee7d20f318cc4b16da089
sannidhij <sannidhi.j0101@gmail.com>
2012-09-25 14:19:44 -0500

    Starting from scratc: deleted all files

--- src/com/twu28/biblioteca/ICommand.java
+++
@@ -1,7 +0,0 @@
-package com.twu28.biblioteca;
-
-import java.util.HashMap;
-
-public interface ICommand {
-    public void execute(Console console);
-}
---
+++ test/com/twu28/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,14 @@
+package com.twu28.biblioteca;
+
+import org.junit.Test;
+
+public class BibliotecaAppTest {
+
+    @Test
+    public void shouldSeeWelcomeMessage () {
+
+
+
+    }
+
+}
